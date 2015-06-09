5bc8a18ada1a3fa8cf11f5c342f2eb1b7a682bcb
quake <quake@chanyouji.com>
2015-06-09 09:39:17 +0800

    add learnrubythehardway

---
+++ slides/learnrubythehardway/ex2.rb
@@ -0,0 +1,13 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by ruby.
+
+puts "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# puts "This won't run."
+
+puts "This will run."
+
+# The # in that code is inside a string,
+# so it will be put into the string until the ending " character is hit. 
+puts "Hi # there."

92cd801bf093b28958c1f16d06e102a18e551c32
quake wang <quake.wang@gmail.com>
2015-06-15 13:54:09 +0800

    add lesson 2

---
+++ slides/learnrubythehardway/ex6.rb
@@ -0,0 +1,8 @@
+types_of_people = 10
+x               = "There are #{types_of_people} types of people."
+binary          = "binary"
+do_not          = "don't"
+y               = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
---
+++ slides/learnrubythehardway/ex7.rb
@@ -0,0 +1,5 @@
+puts "抄课文10次" * 10
+
+w = "This is the left side of..."
+e = "a string with a right side."
+puts w + e
